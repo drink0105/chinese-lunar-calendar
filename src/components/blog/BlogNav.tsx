63cc294f65a7a14f2fae5d8f2b1a9e90d2bac48c
[dyad] <fifth.drink@gmail.com>
2026-04-12 15:00:37 +0700

    [dyad] Making blog available on all devices - wrote 1 file(s)

--- src/components/blog/BlogNav.tsx
+++ src/components/blog/BlogNav.tsx
@@ -6,10 +6,10 @@ const BlogNav = () => {
   const { t } = useTranslation();
 
   return (
-    <div className="hidden lg:block">
+    <div className="block">
       <Link 
         to="/blog" 
-        className="text-sm font-black uppercase tracking-widest text-gray-500 hover:text-primary transition-colors px-4 py-2"
+        className="text-xs md:text-sm font-black uppercase tracking-widest text-gray-500 hover:text-primary transition-colors px-2 md:px-4 py-2"
       >
         {t('nav.blog', 'Blog')}
       </Link>
